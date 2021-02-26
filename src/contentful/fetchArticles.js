09508fed3504be19870c6b60e2ecfdd366c3ec36
martinalevin <martinaalevin@gmail.com>
2021-02-26 10:19:28 +0100

    tokens to env var

--- src/contentful/fetchArticles.js
+++ src/contentful/fetchArticles.js
@@ -3,13 +3,13 @@ import * as Config from './config';
 
 const Contentful = require('contentful');
 const Client = Contentful.createClient({
-    space: Config.SPACE_ID,
-    accessToken: Config.CDAPI_ACCESS_TOKEN,
+    space: process.env.SPACE_ID,
+    accessToken: process.env.CDAPI_ACCESS_TOKEN,
 })
 
 export default (async function getArticles () {
     const entries = await Client.getEntries({
-        content_type: Config.POST_CONTENT_TYPE_ID,
+        content_type: process.env.POST_CONTENT_TYPE_ID,
     })
 
     const articles = _.map(entries.items, item => item.fields);
